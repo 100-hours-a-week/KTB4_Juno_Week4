d7127ee759424751a469c6d7a9feaaec08cc72ae
joohee kim <dev.joohee0916@gmail.com>
2026-07-02 20:22:02 +0900

    fix: 게시글 상세 댓글 작성자 조회 N+1 문제 개선

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Comment;
 import com.example.demo.domain.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByPost(Post post);
 
+    @EntityGraph(attributePaths = "author")
     List<Comment> findAllByPostAndDeletedAtIsNull(Post post);
 }
